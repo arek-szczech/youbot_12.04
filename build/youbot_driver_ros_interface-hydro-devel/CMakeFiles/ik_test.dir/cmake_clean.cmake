d59608ae87f8bd32aaaa148ae3b12ac61bce5d03
Arek Szczech <arek@arek-laptop.(none)>
2017-11-11 13:46:28 +0100

    Nie wiem co sie zmienilo

---
+++ build/youbot_driver_ros_interface-hydro-devel/CMakeFiles/ik_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ik_test.dir/src/examples/ik_test.cpp.o"
+  "/home/arek/youbot/devel/lib/youbot_driver_ros_interface/ik_test.pdb"
+  "/home/arek/youbot/devel/lib/youbot_driver_ros_interface/ik_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ik_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
