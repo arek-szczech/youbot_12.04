7bfa44506e9e09cd9a7816b045de733fc812e7dd
Arek Szczech <arek@arek-laptop.(none)>
2017-11-22 20:59:35 +0100

    Offsety, arm_test

---
+++ build/youbot_driver_ros_interface-hydro-devel/CMakeFiles/arm_keyboard_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/arm_keyboard_control.dir/src/examples/arm_keyboard_control.cpp.o"
+  "/home/arek/youbot/devel/lib/youbot_driver_ros_interface/arm_keyboard_control.pdb"
+  "/home/arek/youbot/devel/lib/youbot_driver_ros_interface/arm_keyboard_control"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/arm_keyboard_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/youbot_driver_ros_interface-hydro-devel/CMakeFiles/fk_test_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fk_test_1.dir/src/examples/fk_test_1.cpp.o"
+  "/home/arek/youbot/devel/lib/youbot_driver_ros_interface/fk_test_1.pdb"
+  "/home/arek/youbot/devel/lib/youbot_driver_ros_interface/fk_test_1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fk_test_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
